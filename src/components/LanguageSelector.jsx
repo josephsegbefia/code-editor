3096e14377de18e15bbb29bd9bddee2e86f7bb3d
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-04-18 00:52:23 +0200

    Complete part 2

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -33,7 +33,7 @@ const LanguageSelector = ({ language, onSelect }) => {
             ))
           }
         </MenuList>
-</Menu>
+      </Menu>
     </Box>
   )
 }
--- src/constants.js
+++ src/constants.js
@@ -10,12 +10,12 @@ export const LANGUAGE_VERSIONS = {
 
 
 export const CODE_SNIPPETS = {
-  javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
-  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
-  python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
-  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
+  javascript: `// Sample code.Replace with your code\n\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
+  typescript: `// Sample code.Replace with your code\n\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
+  python: `# Sample code.Replace with your code\n\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
+  java: `// Sample code.Replace with your code\n\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
   csharp:
-    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
-  php: "<?php\n\n$name = 'Alex';\necho $name;\n",
+    '// Sample code.Replace with your code\nusing System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
+  php: "<?php\n// Sample code.Replace with your code\n\n$name = 'Alex';\necho $name;\n",
   // html: `<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>Your Title Here</title>\n\t<!-- Add any additional meta tags, stylesheets, or scripts here -->\n</head>\n<body>\n\t<!-- Your content goes here -->\n\n\t<!-- Example content -->\n\t<h1>Hello, World!</h1>\n\t<!-- End of example content -->\n\n\t<!-- Add any scripts at the end of the body for better performance -->\n</body>\n</html>\n`,
 };
